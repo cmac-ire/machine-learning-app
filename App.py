8b5ec32112afc2e50dbc9eca4bb8894998b8685e
Cormac Farrelly <cfarr311y@gmail.com>
2023-11-20 20:36:50 +0000

    Initial Commit

---
+++ App.py
@@ -0,0 +1,112 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from keras.models import load_model
+from keras.preprocessing.image import img_to_array, load_img
+import sqlite3
+import streamlit.components.v1 as components
+
+
+# Set page config
+st.set_page_config(page_title="Machine Learning Image Classifier", initial_sidebar_state="collapsed")
+
+# Add styles
+st.markdown("""
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <style>
+        .reportview-container {
+            background-color: #f0f2f6;
+        }
+        .big-font {
+            font-size: 30px !important;
+        }
+    </style>
+    """, unsafe_allow_html=True)
+
+# Load model
+@st.cache(allow_output_mutation=True)
+def load_my_model():
+    return load_model('MedicalML_ResNet.h5')
+
+model = load_my_model()
+
+# Initialize database
+def init_db():
+    con = sqlite3.connect('feedback.db')
+    cursor = con.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS feedback_data (
+            feedback INTEGER
+        )
+    ''')
+    con.commit()
+    return con, cursor
+
+def save_feedback(cursor, feedback):
+    cursor.execute("INSERT INTO feedback_data (feedback) VALUES (?)", (feedback,))
+    con.commit()
+
+con, cursor = init_db()
+
+# Function to predict image
+def predict_image(img_array):
+    predictions = model.predict(img_array)
+    confidence_for_Normal_XRay = predictions[0][0]
+    if confidence_for_Normal_XRay >= 0.5:
+        return "Pneumonia X-Ray", confidence_for_Normal_XRay
+    else:
+        return "Normal X-Ray", 1 - confidence_for_Normal_XRay
+
+# App layout
+st.markdown('# Machine Learning Image Classifier', unsafe_allow_html=True)
+
+st.markdown('### Download Test Images and Instructions:')
+st.markdown('[Click here to download](https://drive.usercontent.google.com/download?id=1GkfpA2lZYBZFjyJVrUJ2JktLJCoQEtKW&export=download&authuser=0&confirm=t&uuid=bbc4d2fa-4d0a-496b-bd0b-f5d4cc66be6a&at=APZUnTXikZlI0iAScVYRFtv-pc8_:1699900643544)')
+
+col1, col2, col3 = st.columns([1, 6, 1])
+
+with col2:
+    uploaded_file = st.file_uploader("📊 Choose an X-ray image.", type=["jpg", "jpeg", "png", "webp"])
+    if uploaded_file:
+        image = load_img(uploaded_file, target_size=(224, 224))
+
+        img_array = img_to_array(image) / 255.0
+        img_array = np.expand_dims(img_array, axis=0)
+
+        # Display the image
+        st.image(image, caption='Succesfully uploaded Image.', use_column_width=True)
+
+        predicted_class, confidence = predict_image(img_array)
+        st.markdown(f"# Predicted Class: **{predicted_class}** with *{confidence * 100:.2f}%* confidence.", unsafe_allow_html=True)
+
+# Feedback section
+st.markdown('## 🌟 I value your feedback!', unsafe_allow_html=True)
+feedback1 = st.slider("Rate your experience (1-10)", 1, 10, key="feedback_slider1")
+
+if st.button("Submit Feedback"):
+    save_feedback(cursor, feedback1)
+    st.success(f"Thank you for your feedback! You rated the app as {feedback1}/10")
+
+if st.button("Show Feedback Summary"):
+    cursor.execute("SELECT feedback, COUNT(*) as count FROM feedback_data GROUP BY feedback")
+    feedback_data = cursor.fetchall()
+    df = pd.DataFrame(feedback_data, columns=["Feedback", "Count"])
+    st.bar_chart(df.set_index("Feedback"))
+
+# Badge and author information
+badge_html = """
+<iframe src="https://cmac-ire.github.io/medicalmlapp/linkedin_badge.html" width="640" height="426" frameborder="0" scrolling="no"></iframe>
+"""
+st.markdown(badge_html, unsafe_allow_html=True)
+
+st.markdown("""
+    <div style="display: flex; align-items: center; margin-top: 20px;">
+        <h3 style="margin: 0;">👨‍💻 By Cormac :) </h3>
+        <a href="https://urldefense.com/v3/__https://twitter.com/Cmac_GN__;!!ODpDvJZr5w!DdZF0JvUVsv3QtWGZvsA79wolrXVe5YvXo6k-gR0RNrsC3f7hVNIousCU4Fm2cHxXXJS-3OZ_AzBE1096ZiE5OsI$" target="_blank" style="margin-left: 10px;">
+            <i class="fab fa-twitter-square" style="font-size: 24px;"></i>
+        </a>
+        <a href="https://urldefense.com/v3/__https://www.instagram.com/cmac_987/__;!!ODpDvJZr5w!DdZF0JvUVsv3QtWGZvsA79wolrXVe5YvXo6k-gR0RNrsC3f7hVNIousCU4Fm2cHxXXJS-3OZ_AzBE1096UT30p10$" target="_blank" style="margin-left: 10px;">
+            <i class="fab fa-instagram" style="font-size: 24px;"></i>
+        </a>
+    </div>
+""", unsafe_allow_html=True)
---
+++ README.md
@@ -0,0 +1,94 @@
+# machine learning app
+
+X-Ray Image Classifier
+
+
+An intuitive and user-friendly web application developed using Python to classify X-ray images and identify if they indicate the presence of pneumonia.
+
+
+X-Ray Classifier Demo
+
+![image](https://github.com/cmac-ire/machine-learning-app)
+
+
+Features
+
+1. Upload & Visualize: Easily upload X-ray images and get a visual preview.
+
+2. Instant Prediction: The app uses a trained deep learning model to make instant predictions on the uploaded images.
+
+3. Feedback System: Users can provide feedback on the predictions, aiding in iterative improvement.
+
+4. Feedback Summary: Users can view a summary of the feedback received for the application.
+
+
+
+Live Demo
+
+The application is hosted on Streamlit. Check out the live app here: https://medicalmlapp-mygqbpp338ft7xqbqsixvs.streamlit.app/
+
+Getting Started
+
+Prerequisites
+
+    Python==3.11.4
+    streamlit==1.25.0
+    keras==2.13.1
+    numpy==1.24.3
+    Pillow==9.5.0
+    h5py==3.9.0
+    tensorflow-cpu==2.13.0
+    pandas==2.0.3
+
+
+Installation & Setup
+
+   1. Clone the repository:
+
+    https://github.com/cmac-ire/machine-learning-app
+
+   2. Install the required packages:
+
+    pip install -r requirements.txt
+
+   3. Run the Streamlit app:
+
+    streamlit run app.py
+    
+The app should now be running on http://localhost:8501/.
+
+
+Usage
+
+Simply upload an X-ray image using the file uploader. The app will display the image, make a prediction, and present the results. Users are encouraged to provide feedback on the prediction.
+Built With
+
+    Streamlit - The web framework utilized.
+    TensorFlow/Keras - Employed for deep learning and predictions.
+    SQLite - Used for storing and summarizing user feedback.
+
+Contributing
+
+Contributions make the open-source community an enriching place to learn, inspire, and innovate. Any contributions to this project are heartily appreciated.
+
+    Fork the Project
+    Create your Feature Branch (git checkout -b feature/AmazingFeature)
+    Commit your Changes (git commit -m 'Add some AmazingFeature')
+    Push to the Branch (git push origin feature/AmazingFeature)
+    Open a Pull Request
+
+License
+
+Distributed under Personal License. See LICENSE for more information.
+
+
+Contact
+
+https://www.linkedin.com/in/cormac-farrelly-b080b9279/
+
+https://twitter.com/Cmac_GN
+
+https://www.instagram.com/cmac_987/
+
+
+Project Repository: https://github.com/cmac-ire/machine-learning-app
