27da1e3a58112e9388e53fa145ab3a6cbb527437
Cormac Farrelly <cfarr311y@gmail.com>
2023-11-23 22:50:58 +0000

    e

--- App.py
+++ App.py
@@ -6,7 +6,6 @@ import sqlite3
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-import matplotlib.pyplot as plt
 import os
 from sklearn.metrics import confusion_matrix, classification_report
 
